d5b6a55326214102333d52f2d5bc67b7bd1d2f99
Mufidz04 <mufidmutashim@gmail.com>
2022-01-05 19:36:17 +0700

    Initialize Project

---
+++ uas_TokoBangunan/src/uas_tokobangunan/Uas_TokoBangunan.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uas_tokobangunan;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+/**
+ *
+ * @author mufid
+ */
+public class Uas_TokoBangunan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+        // TODO code application logic here
+        App_Utama utama = new App_Utama();
+        utama.setVisible(true);
+    }
+    
+}
